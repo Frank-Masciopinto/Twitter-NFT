0233a8400ae009e5a4198965a08198d8095091b2
Francesco <francesco.masciopinto@outlook.com>
2022-04-24 07:29:59 +0200

    Updated Popup

--- public/script/components/popup.jsx
+++ public/script/components/popup.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 class Wallet_ui extends Component {
 
     state = {
         count: 1,
         imageUrl: 'https://picsum.photos/200'
     }
+    styles = {
+        fontSize: 30,
+        fontWeight: "bold"
+    }
 
     render() { 
         return (<div>
+                <link rel="stylesheet" href="css/wallet.css" />
                 <img src={this.state.imageUrl} alt="" />
-                <h1 className="badge badge-primary">{this.formatcount()}</h1>
+                <FontAwesomeIcon icon={faCoffee} />
+                <i className='fa-solid fa-alarm-exclamation'></i>
+                <h1 style={this.styles} className="badge badge-primary">{this.formatcount()}</h1>
                 <button>Create New Wallet</button>
                 </div>);
     }
--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import Wallet_ui from '../public/script/components/popup.jsx';
-//import 'bootstrap/dist/css/bootstrap.css';
+//import '../public/css/wallet.css'
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(
